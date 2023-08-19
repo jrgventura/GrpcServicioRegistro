2389898cd2720b0816f8bef64ee8dc8c30383f25
jrgventura <jorge.venturag@outlook.com>
2023-08-18 22:28:06 -0500

    controlador

---
+++ GrpcAppCliente/Controllers/PruebaController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GrpcServicioRegistro;
+using Grpc.Net.Client;
+
+
+namespace GrpcAppCliente.Controllers
+{
+    public class PruebaController : Controller
+    {
+        
+        private Greeter.GreeterClient greeterClient;
+
+        public async Task<IActionResult> Index() { 
+            return View();  
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(string nombre = "",
+            string apellidos="") {
+            
+            var canal = GrpcChannel.ForAddress("https://localhost:7091/");
+            greeterClient = new Greeter.GreeterClient(canal);
+
+            var helloRequest = new HelloRequest();
+            helloRequest.Nombre = nombre;   
+            helloRequest.Apellidos = apellidos; 
+
+            var mensaje = await greeterClient.SayHelloAsync(helloRequest);
+            ViewBag.mensaje = mensaje.Message;
+            return View();
+
+        }
+
+    }
+}
